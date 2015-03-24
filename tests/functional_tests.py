00ebd335259196615aa351adf6327c0ed9eeed30
AK-1121 <akmail255@gmail.com>
2015-03-24 16:22:29 +0400

    Add some func. tests.

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,5 +1,6 @@
 #import selenium
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,17 +17,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # Title of the page:
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Edith is invited to enter a to-do item strailght away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
         # She types "Buy bananas" into a text box
+        inputbox.send_keys('Buy bananas')
 
         # When she hits enter, the page updates, and now the page lists:
         # "1: Buy bananas" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy bananas' for row in rows)
+        )
 
         # There is still a text box inviting her to add another item.
         # She enters "Make pie with bananas"
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items in her list
 
