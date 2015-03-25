51daf0e99ce53c91d2c43997497b5ac5960e9b4c
AK-1121 <akmail255@gmail.com>
2015-03-25 10:30:48 +0400

    Front page HTML now generated from a template

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -5,8 +5,9 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        #self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
+        #self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -14,6 +15,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith open home page of online to-do app:
         self.browser.get('http://localhost:8000')
+        #browser.get("http://stackoverflow.com")
 
         # Title of the page:
         self.assertIn('To-Do', self.browser.title)
@@ -37,7 +39,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy bananas' for row in rows)
+            any(row.text == '1: Buy bananas' for row in rows),
+            "New to-do item did not appear in table."
         )
 
         # There is still a text box inviting her to add another item.
